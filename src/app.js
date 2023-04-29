b2f9970ae9fae04e09510c75ef5c0b184abcd18b
Anelizi <anelizicarvalho@gmail.com>
2023-04-28 23:39:14 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use(router);
 
 //criar o env DATABASE_URL=mongodb://localhost:27017/moodboard
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
